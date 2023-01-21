bce200c3356859f0289c99a799582f566e298fc9
lorenzobasso <lorenzobasso98@gmail.com>
2023-01-21 22:24:03 +0000

    Run formatter

--- conventus/prisma/seed.ts
+++ conventus/prisma/seed.ts
@@ -1,37 +1,34 @@
-// prisma/seed.ts
+import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker'
 
-import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
-
-// initialize Prisma Client
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 // Number of fake db entities to create
-const numToCreate = 30;
+const numToCreate = 30
 
 async function main() {
   const people = Array.from({ length: numToCreate }).map(() => ({
     firstName: faker.name.firstName(),
     lastName: faker.name.lastName(),
     email: faker.internet.email(),
-  }));
+  }))
 
   for (const person of people) {
     await prisma.person.upsert({
       where: { email: person.email },
       update: {},
       create: person,
-    });
+    })
   }
 
-  console.log(`Successfully created ${numToCreate} people`);
+  console.log(`Successfully created ${numToCreate} people`)
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
   })
   .finally(async () => {
-    await prisma.$disconnect();
-  });
+    await prisma.$disconnect()
+  })

62a627f826fcc62b95f4fa31df33477a09f62073
lorenzobasso <lorenzobasso98@gmail.com>
2023-01-29 19:50:58 +0000

    Add files for location module

--- conventus/src/app.module.ts
+++ conventus/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { LocationsModule } from './locations/locations.module'
 import { PeopleModule } from './people/people.module'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule, PeopleModule],
+  imports: [PrismaModule, PeopleModule, LocationsModule],
   controllers: [AppController],
   providers: [AppService],
 })

b1e053e7c44830fbc1df209cea2b2652b60f69e8
lorenzobasso <lorenzobasso98@gmail.com>
2023-02-09 22:35:15 +0000

    Resource for messages

--- conventus/src/app.module.ts
+++ conventus/src/app.module.ts
@@ -4,11 +4,18 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AvailabilitiesModule } from './availabilities/availabilities.module'
 import { LocationsModule } from './locations/locations.module'
+import { MessagesModule } from './messages/messages.module'
 import { PeopleModule } from './people/people.module'
 import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule, PeopleModule, LocationsModule, AvailabilitiesModule],
+  imports: [
+    PrismaModule,
+    PeopleModule,
+    LocationsModule,
+    AvailabilitiesModule,
+    MessagesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
